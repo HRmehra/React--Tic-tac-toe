a1751adf0bbe5bcc55f1140c74644dac66794470
russell <russellmehra@gmail.com>
2025-12-26 22:44:09 +0530

    main page & player name fnctn

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Player from './components/Player.jsx'
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="player1" symbol = "X"/>
+          <Player initialName="player2" symbol = "O"/>
+        </ol>
+        Game board
+      </div>
+    </main>
+  );
+}
+
+export default App;
