a493165ce872207c9e3f9ef21c322f2fac7c0807
russell <russellmehra@gmail.com>
2025-12-27 09:15:03 +0530

    player name selection and symbol change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import Player from './components/Player.jsx'
 import GameBoard from './components/GameBoard.jsx';
 function App() {
+  const[activePlayer, SetActivePlayer] = useState('X');
+  function  handleSelectSquare(){
+    SetActivePlayer((curActivePlayer)=>curActivePlayer==='X' ? 'O' : 'X');
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="player1" symbol = "X"/>
-          <Player initialName="player2" symbol = "O"/>
+        <ol id="players" className='highlight-player'>
+          <Player initialName="player1" symbol = "X" isActive={activePlayer==='X'}/>
+          <Player initialName="player2" symbol = "O" isActive={activePlayer==='O'}/>
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePLayerSymbol={activePlayer}/>
       </div>
     </main>
   );
