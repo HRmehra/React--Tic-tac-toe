8e2772d9e9565042ec28ece8a95f33e54c378aea
russell <russellmehra@gmail.com>
2025-12-27 08:54:29 +0530

    basic game board and sqaure selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from './components/Player.jsx'
+import GameBoard from './components/GameBoard.jsx';
 function App() {
   return (
     <main>
@@ -7,7 +8,7 @@ function App() {
           <Player initialName="player1" symbol = "X"/>
           <Player initialName="player2" symbol = "O"/>
         </ol>
-        Game board
+        <GameBoard />
       </div>
     </main>
   );
